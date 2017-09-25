379d22cd3990c3adda24e71c30138f43bb2081e2
Aniezka <aniezka.sherbakova@gmail.com>
2017-09-25 21:23:56 +0300

    Добавлены изменения

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,12 +1,11 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if a/b==c:
+a = int(input())
+b = int(input())
+c = int(input())
+if a / b == c:
     print('Обладает свойством 5')
 else:
     print('Не обладает свойством 5')
-if a**b==c:
+if a ** b == c:
     print('Обладает свойством 6')
 else:
     print('Не обладает свойстом 6')
-input()
