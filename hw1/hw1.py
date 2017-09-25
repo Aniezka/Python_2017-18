2d1fe9f65d567b9a2d5b3522f56d4173aaeb1a45
Aniezka <aniezka.sherbakova@gmail.com>
2017-09-25 21:02:54 +0300

    Добавлен файл

---
+++ hw1/hw1.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if a/b==c:
+    print('Обладает свойством 5')
+else:
+    print('Не обладает свойством 5')
+if a**b==c:
+    print('Обладает свойством 6')
+else:
+    print('Не обладает свойстом 6')
+input()
