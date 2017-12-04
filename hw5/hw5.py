3d700d641f97a3616b3d2d8ace6687c32382966a
Aniezka <aniezka.sherbakova@gmail.com>
2017-12-04 21:37:51 +0300

    Добавлен файл

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,6 +1,8 @@
 #Вариант 7
-print ("Input 8 words separated by space")
+print("Input 8 words separated by space")
 w = input().split()
-with open ("output.txt", "w", encoding="utf-8") as file:
-    for i in range (0, 8, 2):
-        file.write (w[i] + w[i + 1] + "\n")
+with open("output.txt", "w", encoding="utf-8") as file:
+    if len(w) < 8:
+        print("word's count < 8")
+    for i in range(0, min(8, len(w)), 2):
+        file.write(w[i] + w[i + 1] + "\n")
