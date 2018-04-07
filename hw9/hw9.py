306b82087274ad39e79db3b32fb159a6da80c448
Aniezka <aniezka.sherbakova@gmail.com>
2018-04-07 10:39:06 +0300

    Добавлен файл

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -1,21 +1,19 @@
 import re
 
 
-inFile = open("Белая гвардия.txt", "r", encoding="utf-8")
+with open("Белая гвардия.txt", "r", encoding="utf-8") as inFile:
 
-reg_expr_list = []
+    reg_expr_list = []
 
-reg_expr_list.append("\s[Сс]ид[а-я]*[лтвеьмс]?[ьаеойия]?|\s[Сс]иж[а-я]") # ...
-reg_expr_string = '|'.join(reg_expr_list)
+    reg_expr_list.append("\s[Сс]ид[а-я]*[лтвеьмс]?[ьаеойия]?|\s[Сс]иж[а-я]") # ...
+    reg_expr_string = '|'.join(reg_expr_list)
 
-words_set = set()
+    words_set = set()
 
-for line in inFile.readlines():
-    all_results = re.findall(reg_expr_string, line)
-    for word in all_results:
-#       print(word[1:])
-        words_set.add(word[1:].lower())
+    for line in inFile.readlines():
+        all_results = re.findall(reg_expr_string, line)
+        for word in all_results:
+    #       print(word[1:])
+            words_set.add(word[1:].lower())
 
-inFile.close()
-
-print(*words_set)
+    print(*words_set)
