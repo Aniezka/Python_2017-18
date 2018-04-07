75f88f5ae152e96322b229c95bd8da33f3048d66
Aniezka <aniezka.sherbakova@gmail.com>
2018-04-07 10:41:05 +0300

    Файл удален

--- kr/1.py
+++
@@ -1,9 +0,0 @@
-#Вариант_2
-#Задание_1
-with open("Ozhegov.txt", "r", encoding="utf-8") as file:
-    a = file.readlines()
-    for i in range (len(a)):
-        k = a[i].find ("|")
-        s = a[i][:k]
-        if len(s) > 20:
-            print(s)
